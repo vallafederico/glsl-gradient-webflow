37ff6e0579213fae13f5eb9b35cee6a800a7ebdd
vallafederico <valla.federico@gmail.com>
2024-04-30 09:29:23 +0200

    test

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
 import Gl from "./modules/gl/gl.js";
 
-export class App {
-  constructor() {
-    this.call = {
-      mode: (name) => {
-        console.log("light mode", name);
-        this.gl.scene.quad.mode(name);
-      },
-      swap: (name, params = { d: 0 }) => {
-        console.log("swap", name, params);
-        this.gl.scene.quad.swap(name, params);
-      }
-    };
+(() => {
+  class App {
+    constructor() {
+      this.call = {
+        mode: (name) => {
+          console.log("light mode", name);
+          this.gl.scene.quad.mode(name);
+        },
+        swap: (name, params = { d: 0 }) => {
+          console.log("swap", name, params);
+          this.gl.scene.quad.swap(name, params);
+        }
+      };
 
-    this.gl = new Gl("c");
+      this.gl = new Gl("c");
+    }
   }
-}
 
-window.Gradient = new App();
+  window.Gradient = new App();
+})();
 
 /** Interface */
 // console.log(window.Gradient.call.mode("light"));
